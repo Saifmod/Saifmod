ebbbacec316d4af363624dad32ff8039ce364516
Saifmod <ssyf87946@gmail.com>
2024-12-22 16:11:35 +0300

    ksjskawj.lua

--- Saifmod.lua
+++ Saifmod.lua
@@ -1,197 +1,50 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "legends of speed", HidePremium = false, SaveConfig = true, ConfigFolder = "@ksjskawj"})
 local Tab = Window:MakeTab({
-	Name = "ايم بوت",
+	Name = "الرئيسيه",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "اهلا بك "
+})
+OrionLib:MakeNotification({
+	Name = "@ksjskawj",
+	Content = "شكرا لاستخدامك سكربتنا / @ksjskawj",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+Tab:AddLabel("شكرا لاستخدامك الى السكربت")
+local Section = Tab:AddSection({
+	Name = ""
+})
+Tab:AddLabel("السكربت مقدم من قناه @ksjskawj")
+local Tab = Window:MakeTab({
+	Name = "اخذ السرعه",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 Tab:AddButton({
-	Name = "ايم بوت ¹",
+	Name = "10 نقطه مجانا",
 	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/HomeMadeScripts/Camlock-aimlock/main/obf_Wxr6QgzF76G1y2Ch77KN4Zt5Nz0A6GIl61gitv3mRR2t3V103al5d0g26s4KY04r.lua.txt"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "ايم بوت ²",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/Ndora1/Ndora1/main/DeepHub"))()
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "كشف اماكن",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "كشف اماكن ¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/UESP'))()
-  	end    
-})
+      		print("button pressed")local args = {
+    [1] = "collectOrb",
+    [2] = "Orange Orb",
+    [3] = "City"
+}
 
+game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+  	end    
+})
+Tab:AddButton({
+	Name = "20 نقطه مجانا",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "collectOrb",
+    [2] = "Red Orb",
+    [3] = "City"
+}
 
-local Tab = Window:MakeTab({
-	Name = "mm2",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "mm2¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
-  	end    
-})
-Tab:AddButton({
-	Name = "mm2²",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2"))()
-  	end    
-})
-
-local Tab = Window:MakeTab({
-	Name = "بلوكس فروت",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "بلوكس فروت ¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "بلوكس فروت ² ميراج",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet('https://raw.githubusercontent.com/REWzaKunGz1/premium/main/PadoHub'))()
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "Pet Simulator",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "Pet Simulator ¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/IdiotHub/Scripts/main/Pet%20Simulator%2099/Hub"))()
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "كينج ليجسي",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "كينج ليجسي¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/ChopLoris/ArcHub/main/main.lua"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "كينج ليجسي²",
-	Callback = function()
-      		print("button pressed")
-getgenv().CustomDistance = 10 -- For Bug Can't Swipe Distance
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ChopLoris/ArcHub/main/main.lua"))()
-  	end    
-})
-
-local Tab = Window:MakeTab({
-	Name = "ماب البيوت",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "ماب البيوت ¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Ice-Hub-6414"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "ماب البيوت ²",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/VR7ss/OMK/main/VR7%20RAGDOLL"))()
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "tps street soccer",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "tps street soccer¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://pastebin.com/raw/TBFtck2B"))()
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "Blade bull",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "Blade bull¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://scriptblox.com/raw/Blade-Ball-Auto-parry-and-more-8515"))()
-  	end    
-})
-local Tab = Window:MakeTab({
-	Name = "gym league",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "gym league¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://ryk.bloxteam.com/loader.lua"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "gym league²",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://ryk.bloxteam.com/loader.lua"))()
-  	end    
-})
-
-local Tab = Window:MakeTab({
-	Name = "سكربتات اخرى",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-Tab:AddButton({
-	Name = "طيران",
-	Callback = function()
-      		print("button pressed")--ARCEUS X FLY V2 SCRIPT
-loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
-  	end    
-})
-Tab:AddButton({
-	Name = "تنقل",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet('https://raw.githubusercontent.com/Ndora1/Ndora1/main/Nokies'))()
-  	end    
-})
-Tab:AddButton({
-	Name = "نسخ سكنات",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))()
-  	end    
-})
-Tab:AddButton({
-	Name = "ادمن ¹",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-  	end    
-})
-Tab:AddButton({
-	Name = "ادمن ²",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet("https://raw.githubusercontent.com/FD2Team/Nameless-Admin-No-Byfron-Kick/main/Source",true))()
-  	end    
-})
-Tab:AddButton({
-	Name = "اختفاء",
-	Callback = function()
-      		print("button pressed")loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
   	end    
 })
