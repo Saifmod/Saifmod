c6c1992d32084697e5b624d9df483b33a50c50ab
Saifmod <ssyf87946@gmail.com>
2024-12-25 21:34:15 +0300

    Saif-@ksjskawj.lua

---
+++ Saif-@ksjskawj.lua
@@ -0,0 +1,219 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "سكنات ماب البيوت", HidePremium = false, SaveConfig = true, ConfigFolder = "@ksjskawj"})
+
+
+
+local Tab = Window:MakeTab({
+	Name = "قسم الشعر",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "شعر 1",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 18310540072
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "شعر 2",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 13935895755
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "شعر 3",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 13935865469
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "شعر 3",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 17057887688
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "شعر 4",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 15568862581
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "شعر 5",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 18985124197
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+local Tab = Window:MakeTab({
+	Name = "اقنعه",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "قناع 1",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 14433195342
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "قناع 2",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 18478393961
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "قناع 3",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 83571501856575
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = " تيشرت",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "تيشيرت 1",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 11627996591
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "تيشيرت 2",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 12792201483
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "تيشيرت 3",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 11905457186
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "تيشيرت 4",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 11781373222
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+local Tab = Window:MakeTab({
+	Name = "سروال",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "سروال 1",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 9155734063
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "سروال 2",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 12531449973
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+Tab:AddButton({
+	Name = "سروال 3",
+	Callback = function()
+      		print("button pressed")local args = {
+    [1] = "wear",
+    [2] = 16480558221
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+
+  	end    
+})
+
